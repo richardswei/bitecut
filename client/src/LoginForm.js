1dd02265f64dd832c7894d24dbe2530857f9548e
THEO <github@theodr.net>
2020-02-15 22:31:44 -0800

    Turn LoginForm back into a functon.
    Demonstrate how it can be used with an action as a prop:
    
    function doLogin() {
      // Do something
    }
    
    <LoginForm action={doLogin} />

--- client/src/LoginForm.js
+++ client/src/LoginForm.js
@@ -3,25 +3,20 @@ import './LoginForm.css'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
-class LoginForm extends React.Component {
-  render() {
-    return (
-      <Form className='LoginForm' preventDefault={true}>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label className='LoginForm-label'>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" className="LoginForm-textline" />
-        </Form.Group>
+const LoginForm = ({action}) =>
+<Form className='LoginForm' onSubmit={(event) => event.preventDefault()||action&&action()}>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label className='LoginForm-label'>Email address</Form.Label>
+  <Form.Control type="email" placeholder="Enter email" className="LoginForm-textline" />
+  </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label className='LoginForm-label'>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" className='LoginForm-textline' />
-        </Form.Group>
-        <Button variant="primary" type="submit" className='LoginForm-submit'>
-          Submit
-        </Button>
-      </Form>
-    )
-  }
-}
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label className='LoginForm-label'>Password</Form.Label>
+  <Form.Control type="password" placeholder="Password" className='LoginForm-textline' />
+  </Form.Group>
+  <Button variant="primary" type="submit" className='LoginForm-submit'>
+    Submit
+  </Button>
+</Form>
 
 export default LoginForm
