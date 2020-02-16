043ba1e48aed8e8470d09df5ac9b51e28f29b157
THEO <github@theodr.net>
2020-02-15 22:07:31 -0800

    Convert to class

--- client/src/LoginForm.js
+++ client/src/LoginForm.js
@@ -3,20 +3,25 @@ import './LoginForm.css'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
-const LoginForm = () =>
-<Form className='LoginForm'>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label className='LoginForm-label'>Email address</Form.Label>
-  <Form.Control type="email" placeholder="Enter email" className="LoginForm-textline" />
-  </Form.Group>
+class LoginForm extends React.Component {
+  render() {
+    return (
+      <Form className='LoginForm' preventDefault={true}>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label className='LoginForm-label'>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" className="LoginForm-textline" />
+        </Form.Group>
 
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label className='LoginForm-label'>Password</Form.Label>
-  <Form.Control type="password" placeholder="Password" className='LoginForm-textline' />
-  </Form.Group>
-  <Button variant="primary" type="submit" className='LoginForm-submit'>
-    Submit
-  </Button>
-</Form>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label className='LoginForm-label'>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" className='LoginForm-textline' />
+        </Form.Group>
+        <Button variant="primary" type="submit" className='LoginForm-submit'>
+          Submit
+        </Button>
+      </Form>
+    )
+  }
+}
 
 export default LoginForm
